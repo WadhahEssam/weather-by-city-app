2541dd0eeb18d1393a5ce11bf72bd0efa74a15d7
Wadah Esam <wadah-wleed@hotmail.com>
2018-08-08 08:59:45 +0300

    add

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,11 @@
 const API_KEY = 'd8bfb85711fc7f4676c4ee982e6d0924' ;
+const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}` ;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER' ;
+
+export function fetchWeather ( cityName ) {
+    return {
+        type : FETCH_WEATHER ,
+        cityName : cityName ,
+    }
+}

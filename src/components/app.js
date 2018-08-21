b623f3f827290b82742cec90f89856e1d92c9610
Wadah Esam <wadah-wleed@hotmail.com>
2018-08-22 00:14:16 +0300

    finish

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import SearchBar from './searchBar';
 import WeatherList from "./weather_list";
 import Logo from './logo' ;
+import Chart from './chart' ;
 
 export default class App extends Component {
   render() {
---
+++ src/components/chart.js
@@ -0,0 +1,72 @@
+import React , { Component } from 'react' ;
+
+
+class Chart extends Component {
+
+    shouldComponentUpdate() {
+        return false;
+    }
+
+
+    componentDidMount () {
+
+
+        console.log( this.refs.name.innerHTML = 'ali' )  ;
+
+        var myChart = new Chart( $(this.refs.wow) , {
+            type: 'bar',
+            data: {
+                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
+
+        console.log (myChart) ;
+
+    }
+
+    render () {
+
+        return (
+            <div>
+                <canvas ref="wow" width="400" height="400" > </canvas>
+                <h1 ref="name" >wadeh</h1>
+            </div>
+        ) ;
+
+    }
+
+
+}
+
+
+export default Chart ;
